96afcbf610b9e3b0e049abf88a4ed8c32c4b36a1
Kechida98 <kechida.ok@gmail.com>
2024-03-27 23:03:06 +0100

    Implement Endgamepress function/print msg to display

--- endgame.ino
+++ endgame.ino
@@ -1,2 +1,51 @@
 void EndGamePress() {
+    lcd.clear(); // Rensa LCD-skärmen
+
+    // Om spelet är aktivt
+    if (gameActive) {
+      lcd.print("Exiting game"); 
+      delay(3000); 
+      lcd.clear(); 
+      lcd.setCursor(0, 0);
+      lcd.print("You left with"); 
+      delay(250); 
+      lcd.setCursor(0, 1);
+      lcd.print("Money = ");
+      lcd.print(playerMoney); 
+      delay(2000); 
+      lcd.clear(); // Rensa LCD-skärmen
+
+      // Skriv ut den viftande texten rad för rad
+      for (int i = 0; i < 3; i++) {
+        lcd.setCursor(0, 2);
+        lcd.print("                "); // Rensa föregående text
+        lcd.setCursor(0, 2);
+        lcd.print("Game reset"); // Visa meddelandet "Game reset"
+        delay(1000); // Vänta i 1 sekund
+        lcd.setCursor(0, 3);
+        lcd.print("                "); // Rensa föregående text
+        lcd.setCursor(0, 3);
+        lcd.print("play again!"); // Visa meddelandet "play again!"
+        delay(1000); // Vänta i 1 sekund
+      }
+
+      resetGameState(); // Återställ spelets tillstånd
+      gameActive = false; // Markera att spelet inte längre är aktivt
+    } else { // Om spelet inte är aktivt
+      if (!gameStarted) { // Om spelet ännu inte har börjat
+        lcd.print("Goodluck!"); // Visa meddelandet "Goodluck!"
+        delay(2000); 
+        lcd.clear(); 
+        lcd.setCursor(0, 0);
+        lcd.print("Current saldo:"); 
+        lcd.setCursor(0, 1);
+        lcd.print(playerMoney); 
+        delay(3000);
+        lcd.clear(); 
+
+        gameStarted = true; // Markera att spelet har börjat
+      }
+
+      gameActive = true; // Markera att spelet är aktivt
+    }
 }
