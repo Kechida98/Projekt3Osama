8d8cb9f3dbf2ef07fb4cc8a4fff390a41ccaa7e1
Kechida98 <kechida.ok@gmail.com>
2024-03-27 21:15:23 +0100

    Handle button presses while playerbuttonhigh

--- rolldice.ino
+++ rolldice.ino
@@ -4,4 +4,11 @@ void rollDiceAndHandleResult() {
   lcd.setCursor(0, 1);
   lcd.print("Current bet: ");
   lcd.print(bet); // Visa det aktuella satsbeloppet
+  
+  // Loop för att hantera knapptryckningar medan spelarknappen är nedtryckt
+  while (digitalRead(playerButtonPin) == HIGH) {
+    EndGamePress(); // Hantera knapptryckning för att avsluta spelet
+    IncreaseBetPress(); // Hantera knapptryckning för att öka satsningen
+    DecreaseBetPress(); // Hantera knapptryckning för att minska satsningen
+  }
 }
