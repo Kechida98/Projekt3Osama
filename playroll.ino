b565f6844f022d8219db2f43d05e574cbd5116c4
Kechida98 <kechida.ok@gmail.com>
2024-03-27 20:59:09 +0100

    Check if player button is pressed, player has enough money, and debounce

--- playroll.ino
+++ playroll.ino
@@ -1,3 +1,7 @@
 // Funktion för att hantera spelarens rullknapp
 void PlayerRoll() {
-}
+  // Kontrollera om spelaren har tryckt på rullknappen och har tillräckligt med pengar och undvik skakningar
+  if (digitalRead(playerButtonPin) == HIGH && playerMoney > 0 && millis() - lastDebounceTimePlayer > debounceDelay) {
+    lastDebounceTimePlayer = millis(); // Uppdatera tiden för senaste knapptryckning
+ } 
+} 
