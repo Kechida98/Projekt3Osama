df6e0cef813af6987408dc5f229fff313b812052
Kechida98 <kechida.ok@gmail.com>
2024-03-27 21:12:25 +0100

    Print messages and display current bet on lcd

--- rolldice.ino
+++ rolldice.ino
@@ -1,2 +1,7 @@
 void rollDiceAndHandleResult() {
+  lcd.clear(); // Rensa LCD-skärmen
+  lcd.print("Choose bet"); // Visa meddelande för att välja sats
+  lcd.setCursor(0, 1);
+  lcd.print("Current bet: ");
+  lcd.print(bet); // Visa det aktuella satsbeloppet
 }
