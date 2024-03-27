5adfb192efcb81ef8004a86d00d23b411959425c
Kechida98 <kechida.ok@gmail.com>
2024-03-27 21:00:54 +0100

    Clear LCD, print message, and wait for player to release button

--- playroll.ino
+++ playroll.ino
@@ -3,5 +3,11 @@ void PlayerRoll() {
   // Kontrollera om spelaren har tryckt på rullknappen och har tillräckligt med pengar och undvik skakningar
   if (digitalRead(playerButtonPin) == HIGH && playerMoney > 0 && millis() - lastDebounceTimePlayer > debounceDelay) {
     lastDebounceTimePlayer = millis(); // Uppdatera tiden för senaste knapptryckning
+
+    lcd.clear(); 
+    lcd.print("Press to roll"); 
+
+    // Vänta tills spelaren släpper knappen för att rulla tärningarna
+    while (digitalRead(playerButtonPin) == LOW);
  } 
 } 
