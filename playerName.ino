b865bea305b9e069b7c6e5a1e944878d73deb054
Kechida98 <kechida.ok@gmail.com>
2024-03-27 22:29:56 +0100

    Initalize getplayerName function/debounce and printmsg

--- playerName.ino
+++ playerName.ino
@@ -1,3 +1,10 @@
 // Funktion för att låta spelaren mata in sitt namn
 void getPlayerName() {
+   lcd.clear(); // Rensa LCD-skärmen
+  lcd.setCursor(0, 0);
+  lcd.print("Whatis YOU NAME:");
+  lcd.setCursor(0, 1);
+
+  unsigned long lastDebounceTime = 0;
+  const unsigned long debounceDelay = 50;  // Justera debounce-förseningen vid behov
 }
