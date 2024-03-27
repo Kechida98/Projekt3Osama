692985e05dacf9bad9fd569c882f947e9569f266
Kechida98 <kechida.ok@gmail.com>
2024-03-27 20:47:49 +0100

    Check if player can bet and game is active

--- loopfunc.ino
+++ loopfunc.ino
@@ -4,4 +4,11 @@ void loop() {
     getPlayerName(); // Anropa funktionen för att få spelarens namn
     return;
   }
+
+  // Kontrollera om spelaren kan satsa och spelet är aktivt
+  if (canBet && gameActive) {
+    EndGamePress();     // Hantera knapptryckning för att avsluta spelet
+    IncreaseBetPress(); // Hantera knapptryckning för att öka satsningen
+    DecreaseBetPress(); // Hantera knapptryckning för att minska satsningen
+  }
 }
