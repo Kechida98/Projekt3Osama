9cd94b429c27b71ebafe01435a3ea499ddd1c187
Kechida98 <kechida.ok@gmail.com>
2024-03-27 20:15:07 +0100

    Set led pins as Outputs

--- setupinitial.ino
+++ setupinitial.ino
@@ -1,6 +1,2 @@
-pinMode(playerButtonPin, INPUT_PULLUP); // Sätt spelarknappen som en input med intern pull-up resistor
-  pinMode(increaseBetPin, INPUT_PULLUP);  // Sätt knappen för att öka satsningen som en input med intern pull-up resistor
-  pinMode(decreaseBetPin, INPUT_PULLUP);  // Sätt knappen för att minska satsningen som en input med intern pull-up resistor
-  pinMode(endGamePin, INPUT_PULLUP);      // Sätt knappen för att avsluta spelet som en input med intern pull-up resistor
-  pinMode(ledWin, OUTPUT);                // Sätt vinnande LED som en output
-  pinMode(ledLose, OUTPUT);               // Sätt förlorande LED som en output
+pinMode(ledWin, OUTPUT);                // Sätt vinnande LED som en output
+pinMode(ledLose, OUTPUT);               // Sätt förlorande LED som en output
